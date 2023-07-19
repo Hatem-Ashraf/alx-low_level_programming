610690e3d552167dac2b16e452c8517f6b6432a2
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-18 19:47:27 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,18 +17,22 @@ void times_table(void)
 			{
 				_putchar(res / 10 + '0');
 				_putchar(res % 10 + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			} else
 			{
 				_putchar((res) + '0');
-			}
-			if (j < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 			}
 		}
-		_putchar('$');
 		_putchar('\n');
 	}
 	_putchar ('\n');
