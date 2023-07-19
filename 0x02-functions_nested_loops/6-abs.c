771aaa1dd4e0af6d476a99260ea88c81b8d986dc
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-18 18:49:00 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _abs - return the absolute of a number
+  * @n: number to be processed
+  *
+  * Return: the number after processing
+  */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	return (n);
+}
