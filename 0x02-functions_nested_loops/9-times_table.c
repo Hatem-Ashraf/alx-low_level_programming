94c8f8cd4885408b66bf50db602aead4a3b8f2f2
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-19 10:33:07 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,14 +7,14 @@
 void times_table(void)
 {
 	int i, j, res;
-	
+
 	res = j = i = 0;
 	while (1)
 	{
 		res = i * j;
 		if (res > 9)
 		{
-			if (i != 0 && j != 0)
+			if (i != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -23,7 +23,7 @@ void times_table(void)
 			_putchar(res % 10 + '0');
 		} else
 		{
-			if (i != 0 && j != 0)
+			if (i != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
