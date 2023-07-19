37c94c1da2e49230d1c291dec08ed9eec09970d6
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-18 19:03:31 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * print_last_digit - as it is
+  * @n: num
+  *
+  * Return: the n
+  */
+int print_last_digit(int n)
+{
+	int rem;
+
+	if (n < 0)
+	{
+		n  = n * -1;
+	}
+	rem = n % 10;
+	return (rem);
+}
