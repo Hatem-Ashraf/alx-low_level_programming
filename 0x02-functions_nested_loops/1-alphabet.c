54169869eb0585b31ab4fb6afa6cb987d05d28fe
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-18 17:27:38 -0700

    alphabet v3.0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  * print_alphabet = printing alphabet
+  * print_alphabet - printing alphabet
   *
   * Return: nothing
   */
@@ -8,10 +8,10 @@ void print_alphabet(void)
 {
 	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
+
 	for (i = 0; i < 26; i++)
 	{
 		_putchar(alpha[i]);
 	}
 	_putchar('\n');
-	return ;
 }
