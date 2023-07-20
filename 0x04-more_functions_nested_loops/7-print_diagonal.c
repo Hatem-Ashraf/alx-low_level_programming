8ba8d3cce1d6cb661971b89e6895b6757c56bf3c
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-20 16:48:10 -0700

    \ n times

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_diagonal - prints \ n times
+  * @n: No of \
+  *
+  * Return: nothing
+  */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = n; i > 0; i--)
+	{
+		_putchar('\');
+	}
+	_putchar('\');
+}
