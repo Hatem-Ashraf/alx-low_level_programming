d33b272d08789332c727bea6eba2c32bef387647
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-20 16:58:53 -0700

    diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,11 +8,19 @@
   */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
-	for (i = n; i > 0; i--)
+	if (n > 0)
 	{
-		_putchar('\\');
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 	_putchar('\n');
 }
