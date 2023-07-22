a9c706d3db92e60e06e6d414e0b23df04fd5c1b7
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-21 18:57:24 -0700

    largest prime number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,28 +1,5 @@
 #include <stdio.h>
-#include <math.h>
-/**
- * isPrime - check if prime
- * @n: number
- *
- * Return: 1 if prime, 0 otherwise
- */
-int isPrime(long int n)
-{
-	long int i;
 
-	if (n < 2)
-	{
-		return (0);
-	}
-	for (i = 2; i < (long int) pow(n, 0.5) + 1; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
 /**
  * main - start point
  *
@@ -30,18 +7,20 @@ int isPrime(long int n)
  */
 int main(void)
 {
-	long int i;
-	long int x = 612852475143;
-	long int largestPrime = 2;
+	long long int i, x = 612852475143, largestPrime = 2;
 
-	for (i = 2; i < x; i++)
+	for (i = 2; i * i <= x; i++)
 	{
-		if (isPrime(i) && x % i == 0)
+		while (x % i == 0)
 		{
-			x = x / i;
 			largestPrime = i;
+			x /= i;
 		}
 	}
-	printf("%ld\n", largestPrime);
+	if (x > largestPrime)
+	{
+		largestPrime = x;
+	}
+	printf("%lld\n", largestPrime);
 	return (0);
 }
