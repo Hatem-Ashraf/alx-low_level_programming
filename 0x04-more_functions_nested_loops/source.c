abc8697685325e56fbc141c34a39bd0923b6035c
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-24 14:26:07 -0700

    reset to 98

--- 0x04-more_functions_nested_loops/source.c
+++ 0x04-more_functions_nested_loops/source.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <limits.h>
 /**
  * main - check the code
  *
@@ -17,5 +17,8 @@ int main(void)
     _putchar('\n');
     print_number(-98);
     _putchar('\n');
+    print_number(INT_MIN);
+    _putchar('\n');
+
     return (0);
 }
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * reset_to_98 - update the value of a param to be 98
+  * @n: pointer to int
+  *
+  * Return: nothing
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
