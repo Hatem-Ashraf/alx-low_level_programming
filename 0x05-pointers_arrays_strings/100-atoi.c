51c84f7ab1e7160e1187a8147039983c2f4d10b2
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-25 13:35:30 -0700

    atoi()

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,7 +12,7 @@ int _atoi(char *s)
 {
 	char *start = s;
 	int flag = 1;
-	int val;
+	unsigned int val;
 
 	while (*start != '\0')
 	{
@@ -31,7 +31,10 @@ int _atoi(char *s)
 		return (0);
 	}
 	val = atoi(start);
-	if (flag == -1)
+	if (flag == -1 && val == 2147483648)
+	{
+		val = ~val + 1;
+	} else if (flag == -1 )
 	{
 		val *= -1;
 	}
