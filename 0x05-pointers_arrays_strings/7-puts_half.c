e9def03fd891ff2bd583393c2f49e755f6113ca5
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-24 18:39:19 -0700

    2nd half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * puts_half - prints the 2nd half of the string
+  * @str: string
+  *
+  * Return: nothing
+  */
+void puts_half(char *str)
+{
+	char *s = str;
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	if (len % 2 != 0)
+	{
+		len = (len - 1) / 2;
+	} else
+	{
+		len /= 2;
+	}
+	s = str + len;
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+}
