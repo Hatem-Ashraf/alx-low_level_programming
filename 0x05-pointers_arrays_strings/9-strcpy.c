020f205499c656d2d196c63fe232a2b7bf625647
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-24 19:35:45 -0700

    strcpy()

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+  * _strcpy - copy
+  * @dest: dest str
+  * @src: src str
+  *
+  * Return: pointer to char
+  */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+
+}
