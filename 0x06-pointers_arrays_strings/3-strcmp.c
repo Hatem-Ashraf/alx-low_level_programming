300d64036bda2542d1b7bc8245207a1f2b4f5db8
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-27 13:18:59 -0700

    strcmp()

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+/**
+  * _strcmp - compares 2 stings
+  * @s1: string 1
+  * @s2: string 2
+  *
+  * Return: +ve if the s1 > s2 or -ve if s1 < s2 or 0 otherwise
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
