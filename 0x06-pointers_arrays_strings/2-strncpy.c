f2da82e1e7b42d53cfc2de9306481d95070bfca7
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-27 13:09:30 -0700

    strnpcy()

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "main.h"
 
 /**
@@ -8,23 +9,7 @@
   *
   * Return: the dest str
   */
-
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *temp1 = dest;
-	char *temp2 = src;
-	int i = 1;
-
-	while (i <= n && *temp2 != '\0')
-	{
-		*temp1 = *temp2;
-		i++;
-		temp1++;
-		temp2++;
-	}
-	if (*temp2 == '\0')
-	{
-		*temp1 = '\0';
-	}
-	return (dest);
+	return (strncpy(dest, src, n));
 }
