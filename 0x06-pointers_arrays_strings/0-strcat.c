28a6f8f407c1989274ac82129d6bb7fb6de30119
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-27 12:23:25 -0700

    strcat()

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+/**
+  * _strcat - concatinate 2 strings
+  * @dest: the destination string
+  * @src: the source string
+  *
+  * Return: pointer to that concatinated string
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
