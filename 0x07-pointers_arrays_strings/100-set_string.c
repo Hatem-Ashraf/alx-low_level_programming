d697c3838a371029331adf1940e9f8ba3508a274
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-07-31 19:21:20 -0700

    double pointer exchange value

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * set_string - sets the s to be the to
+  * @s: pointer to pointer to char
+  * @to: pointer to char
+  *
+  * Return: nothing
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
