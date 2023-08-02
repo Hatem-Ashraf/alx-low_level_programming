027b44c2ca3d19a9849c2934f5fe11f7e0b49727
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-02 15:50:17 -0700

    puts()

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _puts_recursion - prints string to the stdout
+  * @s: string
+  *
+  * Return: nothing
+  */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
