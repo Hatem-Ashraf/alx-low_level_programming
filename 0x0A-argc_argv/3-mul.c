36547e3e3be7dcdfce7b1a86b1c0695fecd736f3
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-04 18:16:02 -0700

    mul 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,12 +9,29 @@
   */
 int main(int argc, char *argv[])
 {
+	int n1, n2;
+
 	(void) argc;
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", (*argv[1] - 48) * (*argv[2] - 48));
+	if (*argv[1] == 45)
+	{
+		n1 =  -1 * (*(argv[1] + 1) - 48);
+	} else
+	{
+		n1 = *argv[1] - 48;
+	}
+	if (*argv[2] == 45)
+	{
+		n2 =  -1 * (*(argv[2] + 1) - 48);
+	} else
+	{
+		n2 = *argv[2] - 48;
+	}
+
+	printf("%d\n", n1 * n2);
 	return (0);
 }
