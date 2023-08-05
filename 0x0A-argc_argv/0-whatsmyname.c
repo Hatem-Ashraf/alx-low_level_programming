f301ca7b32f766ff978824a98a9aab14a20fec52
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-04 17:28:41 -0700

    program's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#
+
+/**
+  * main - start point
+  * @argc: args count
+  * @argv: args values
+  *
+  * Return: always 0
+  */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", *argv);
+	return (0);
+}
