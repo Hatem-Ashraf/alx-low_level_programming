b1d67038e083d9d3c290cf84d8ddf6e397b8cbbb
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-11 07:39:46 -0700

    int array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,9 +11,14 @@ int *array_range(int min, int max)
 {
 	int *arr = NULL;
 	int i;
-
+	int range = max - min + 1;
 	if (min > max)
 	{
+		return (NULL);
+	}
+	if (range <= 0)
+	{
+		/*if overflow occures*/
 		return (NULL);
 	}
 	arr = malloc((max - min + 1) * sizeof(int));
