0b6ea67465acf0557b48aaead3086635fd4c7b28
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-11 07:11:36 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -45,7 +45,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	len1 = ln(str1);
 	len2 = ln(str2);
-	if (n >= len2)
+	if (n > len2)
 		n = len2;
 	s = malloc((len1 + n + 1) * sizeof(char));
 	if (!s)
