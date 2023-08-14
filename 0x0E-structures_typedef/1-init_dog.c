df15439b72991793f6e1652a5106a85e455ecafa
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-14 12:19:41 -0700

    init a dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,10 +1,20 @@
 #include "dog.h"
 #include <stdio.h>
 /**
-  * init_dog - 
+  * init_dog - initiate a dog struct
+  * @d: dog struct
+  * @name: dog's name
+  * @age: age
+  * @owner: owner
+  *
+  * Description: assign values to the a dog
   */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (!d)
+	{
+		return;
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
