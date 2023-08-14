fc0bf7f508f5e9b152538ba0b07ba6ec59089ad3
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-14 12:34:20 -0700

    print a dog's infoswq

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+  * print_dog - prints a dog values if exist
+  * d: dog struct
+  *
+  * Description: a function to print the dog's values
+  */
+void print_dog(struct dog *d)
+{
+	if (!d)
+	{
+		return;
+	}
+	if (!d->name)
+	{
+		printf("Name: (nil)\n");
+	} else {
+		printf("Name: %s\n", d->name);
+	}
+	if(!d->age)
+	{
+		printf("Age: (nil)\n");
+	} else {
+		printf("Age: %f\n", d->age);
+	}
+	if (!d->owner)
+	{
+		printf("Owner: (nil)\n");
+	} else {
+		printf("Owner: %s\n", d->owner);
+	}
+}
