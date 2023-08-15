502c6f06a6343042e298c5da4c4f5d0b148d1ca9
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-14 19:27:34 -0700

    ABS()

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,6 @@
-#ifndef LI_H
-#define LI_H
-	#include <limits.h>
-#endif
-
 #ifndef MAIN_H_3
 #define MAIN_H_3
 
-#define ABS(x) (((x) > 0 ? (x) : ((x) == INT_MIN) ? (~(x)) : (-x)))
+#define ABS(x) ((x) > 0 ? (x) : (-x))
 
 #endif
