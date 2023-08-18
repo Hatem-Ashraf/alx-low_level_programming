58a32a3ca1c1f8ed0d1e1801e556705bc872712c
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-17 17:24:42 -0700

    print all params with separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+  * print_numbers - prints all the params seperated
+  * @separator: the seperator between each param
+  * @n: number of params
+  *
+  * Return: nothing
+  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (!separator)
+	{
+		return;
+	}
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d%s", va_arg(ap, int), i != n - 1 ? separator : "\n");
+	}
+}
