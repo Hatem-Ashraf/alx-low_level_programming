1ffd159fa60582d65e6dcb9f61684981cb444c26
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-17 17:29:38 -0700

    print all params with separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,6 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d%s", va_arg(ap, int), i != n - 1 ? separator : "\n");
+		printf("%d%s", va_arg(ap, int), i != n - 1 ? separator : "");
 	}
+	printf("\n");
 }
