368027d39cc3767aee9cbd73501a467cd4ce4257
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-17 18:25:47 -0700

    print separated strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,16 +12,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *s = NULL;
 
-	if (!separator)
-	{
-		printf("\n");
-		return;
-	}
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(ap, char *);
-		printf("%s%s", s ? s : "(nil)", i != n - 1 ? separator : "");
+		printf("%s%s", s ? s : "(nil)",
+				i != n - 1 ? (separator ? separator : "") : "");
 	}
 	printf("\n");
 	va_end(ap);
