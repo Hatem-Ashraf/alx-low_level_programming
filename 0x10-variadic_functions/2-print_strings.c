11f485645f2baa5f5a1fb85adc0c74f378c9f015
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-17 18:06:40 -0700

    ptrint separated strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+  * print_strings - prints strings separated by the separator
+  * @separator: the patern of separation
+  * @n: No of params
+  *
+  * Return: nothing
+  */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *s = NULL;
+
+	if (!separator)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(ap, char *);
+		printf("%s%s", s ? s : "(nil)", i != n - 1 ? separator : "");
+	}
+	printf("\n");
+	va_end(ap);
+}
