33d951191e31d28ad36e4de4c3a3f484402de79b
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-17 18:20:25 -0700

    print separated int

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,15 +12,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
-	if (!separator)
-	{
-		printf("\n");
-		return;
-	}
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d%s", va_arg(ap, int), i != n - 1 ? separator : "");
+		printf("%d%s", va_arg(ap, int), i != n - 1 ? (separator ? separator : "") : "");
 	}
 	printf("\n");
 	va_end(ap);
