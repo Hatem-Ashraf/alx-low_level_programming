7bee5beb85aeef5a8bfd8a2ef8147e8c89d4089b
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-29 17:56:16 -0700

    insert node at idx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (node);
 	}
 	if (!*head)
+	{
+		free(node);
 		return (NULL);
+	}
 	for (i = 0; temp; i++)
 	{
 		if (i == idx)
