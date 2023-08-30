6eeafaf52c05bb8121d743c6d44b9a053fad26a6
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-29 18:10:57 -0700

    insert node at idx v2.0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,37 +10,31 @@
   */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp, *prev;
+	listint_t *temp;
 	unsigned int i;
 	listint_t *node = malloc(sizeof(listint_t));
 
 	if (!head || !node)
 	{
-		free(node);
 		return (NULL);
 	}
 	temp = *head;
 	node->n = n;
-	if (!*head && idx == 0)
+	node->next = NULL;
+	if (!idx == 0)
 	{
-		node->next = NULL;
+		node->next = *head;
 		*head = node;
 		return (node);
 	}
-	if (!*head)
-	{
-		free(node);
-		return (NULL);
-	}
 	for (i = 0; temp; i++)
 	{
-		if (i == idx)
+		if (i == idx - 1)
 		{
-			prev->next = node;
-			node->next = temp;
+			node->next = temp->next;
+			temp->next = node;
 			return (node);
 		}
-		prev = temp;
 		temp = temp->next;
 	}
 	free(node);
