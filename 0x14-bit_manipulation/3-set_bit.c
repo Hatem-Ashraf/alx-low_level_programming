1ae7b4685fcd22e38095f49e98873740edbda812
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-31 19:04:52 -0700

    Set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * set_bit - Sets a bit
+  * @n: a Number
+  * @index: index
+  *
+  * Return: 1 if success, otherwise -1
+  */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	*n = *n | (1 << index);
+	return (1);
+}
