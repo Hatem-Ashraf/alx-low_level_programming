9bb83d50fd055a1bf859b20a642b458a36c7a6f3
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-09-04 18:16:27 -0700

    create a file and write on it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,8 +25,6 @@ int create_file(const char *filename, char *text_content)
 		;
 	if (text_content)
 		bytes = write(fd, &text_content[0], i);
-	else
-		bytes = write(fd, '\0', 1);
 	if (bytes == -1)
 		return (-1);
 	close(fd);
