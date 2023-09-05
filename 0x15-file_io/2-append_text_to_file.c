0e1ed4b0d60fd0e7067d1099bf7744c4d6bd8b44
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-09-04 18:38:33 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,6 +16,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (access(filename, F_OK) == -1)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 	if (text_content)
 	{
 		for (len = 0; text_content[len]; len++)
