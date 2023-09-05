0ae6135b5ab6062b24dbb76733dd12c38df60dde
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-09-04 17:45:37 -0700

    read and write to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
-  * 
+  * read_textfile - reads a text from a file
+  * @filename: filename
+  * @letters: number of chars to be read
+  *
+  * Return: No of chars printed or 0 if fail
   */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -12,10 +16,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	bytes = read(fd, buffer, letters);
+	bytes = read(fd, &buffer[0], letters);
 	if (bytes == -1)
 		return (0);
-	bytes = write(fd, buffer, bytes);
+	bytes = write(fd, &buffer[0], bytes);
 	if (bytes == -1)
 		return (0);
 	close(fd);
